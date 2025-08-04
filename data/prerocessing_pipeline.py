6f287c66048e4b0977c0159fcd87e517c77a839f
b3rian <brayann.8189@gmail.com>
2025-08-04 15:05:12 +0300

    initial commits

--- data/prerocessing_pipeline.py
+++ data/prerocessing_pipeline.py
@@ -6,3 +6,16 @@ from tensorflow.keras import layers
 # constants
 SEQ_LEN = 128
 BATCH_SIZE = 64
+
+def load_and_clean_lines(file_path, min_words=5, max_words=250):
+    """
+    Load and clean lines from a given text file.
+
+    Args:
+        file_path (str): Path to the text file.
+        min_words (int): Minimum number of words per line.
+        max_words (int): Maximum number of words per line.
+
+    Returns:
+        list: A list of cleaned text lines.
+    """
