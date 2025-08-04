8fa66e48f7b183dc32e44699d4fbd9150d0bd6f6
b3rian <brayann.8189@gmail.com>
2025-08-04 15:02:31 +0300

    initialcommits

--- data/prerocessing_pipeline.py
+++ data/prerocessing_pipeline.py
@@ -1,4 +1,8 @@
 import os
 import tensorflow as tf
-import keras_nlp as keras_hub  # Make sure keras-nlp is installed
+import keras_nlp as keras_hub
 from tensorflow.keras import layers
+
+# constants
+SEQ_LEN = 128
+BATCH_SIZE = 64
