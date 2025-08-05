fccfa1fedbd1dfc4602b5fc13cf8180f74495da0
b3rian <brayann.8189@gmail.com>
2025-08-05 11:03:48 +0300

    initialommits

--- models/token_position_embedding.py
+++ models/token_position_embedding.py
@@ -1,3 +1,14 @@
 from tensorflow import keras
 from keras import ops
 from keras import layers
+
+class TokenAndPositionEmbedding(layers.Layer):
+    """
+    Combines token and positional embeddings.
+
+    This layer learns:
+    - An embedding vector for each token in the vocabulary.
+    - An embedding vector for each position in the input sequence.
+    
+    The final embedding is a sum of the token embedding and the positional embedding.
+    """
