664fef4f1dd9a7e26cc75f9b18800fdfd74cd35d
b3rian <brayann.8189@gmail.com>
2025-08-05 11:06:12 +0300

    initial commits

--- models/token_position_embedding.py
+++ models/token_position_embedding.py
@@ -12,3 +12,15 @@ class TokenAndPositionEmbedding(layers.Layer):
     
     The final embedding is a sum of the token embedding and the positional embedding.
     """
+    def __init__(self, maxlen: int, vocab_size: int, embed_dim: int):
+        """
+        Initializes the token and position embedding layers.
+
+        Args:
+            maxlen (int): Maximum length of the input sequences.
+            vocab_size (int): Size of the vocabulary.
+            embed_dim (int): Dimension of the embedding vectors.
+        """
+        super().__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
