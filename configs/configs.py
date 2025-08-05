eb0b52eb2c9c4e8346a7ff2eec946535bcb3f7fe
b3rian <brayann.8189@gmail.com>
2025-08-05 11:59:42 +0300

    initial commits

---
+++ configs/configs.py
@@ -0,0 +1,17 @@
+# configs.py
+
+import yaml
+
+class Config:
+    def __init__(self, config_path='config.yaml'):
+        with open(config_path, 'r') as file:
+            self.cfg = yaml.safe_load(file)
+
+        # Flatten namespaces for easier access
+        self.model = self.cfg.get("model", {})
+        self.training = self.cfg.get("training", {})
+        self.tokens = self.cfg.get("tokens", {})
+        self.paths = self.cfg.get("paths", {})
+
+# Initialize once and reuse
+config = Config()
