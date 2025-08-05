eaa0a2b8dc50e96e5418ebe97fd3a08e3742214f
b3rian <brayann.8189@gmail.com>
2025-08-05 11:21:32 +0300

    initial commits

--- models/transformer_decoder_model.py
+++ models/transformer_decoder_model.py
@@ -2,3 +2,18 @@ from tensorflow import keras
 from keras import layers, ops
 from models.transformer_decoder_block import TransformerBlock
 from models.embeddings import TokenAndPositionEmbedding
+
+def create_model(maxlen, vocab_size, embed_dim, num_heads, feed_forward_dim):
+    """
+    Builds and compiles a simple transformer-based language model.
+
+    Args:
+        maxlen (int): Maximum sequence length.
+        vocab_size (int): Size of the vocabulary.
+        embed_dim (int): Dimension of token and position embeddings.
+        num_heads (int): Number of attention heads in the transformer block.
+        feed_forward_dim (int): Dimension of the feed-forward network.
+
+    Returns:
+        keras.Model: Compiled Keras model ready for training.
+    """
