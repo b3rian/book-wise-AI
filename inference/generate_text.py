28ceae57e39be7c5e5aaf3f9cadf19b48814b6ee
b3rian <brayann.8189@gmail.com>
2025-08-07 17:38:59 +0300

    initial commits

--- inference/generate_text.py
+++ inference/generate_text.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = Groq(
-    api_key=os.environ.get("GROQ_API_KEY"),
+    api_key= os.environ.get("GROQ_API_KEY"),
 )
 
 chat_completion = client.chat.completions.create(
