d9107b3d3f3e164c5db89b104b34b416ae909004
b3rian <brayann.8189@gmail.com>
2025-08-08 13:37:42 +0300

    initial commits

--- vector_DB/faiss_hf_test.py
+++ vector_DB/faiss_hf_test.py
@@ -4,3 +4,10 @@ import numpy as np
 
 # 1. Load Hugging Face embedding model
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
+# 2. Example documents
+docs = [
+    "God is dead. God remains dead. And we have killed him.",
+    "He who has a why to live can bear almost any how.",
+    "To live is to suffer, to survive is to find some meaning in the suffering."
+]
