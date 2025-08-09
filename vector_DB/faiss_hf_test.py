2584c0af50ff57d8fe99a0daeb19b241fa1b55f5
b3rian <brayann.8189@gmail.com>
2025-08-09 16:52:30 +0300

    initialcommits

--- vector_DB/faiss_hf_test.py
+++ vector_DB/faiss_hf_test.py
@@ -5,7 +5,7 @@ collection = chromadb_client.create_collection(name="test_collection")
 
 collection.add(
     ids=["doc1", "doc2", "doc3"],
-    docs = [
+    documents = [
     "God is dead. God remains dead. And we have killed him.",
     "He who has a why to live can bear almost any how.",
     "To live is to suffer, to survive is to find some meaning in the suffering."
