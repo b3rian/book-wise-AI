a801d750c45081815362ac12d62b8c895d495fbe
b3rian <brayann.8189@gmail.com>
2025-08-11 09:25:51 +0300

    initial commits

--- vector_DB/chroma_db.py
+++ vector_DB/chroma_db.py
@@ -1,26 +1,18 @@
 import chromadb
-from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
 chromadb_client = chromadb.Client()
 collection = chromadb_client.create_collection(name="test_collection")
 
-docs =[
+collection.add(
+    ids=["doc1", "doc2", "doc3"],
+    documents = [
     "God is dead. God remains dead. And we have killed him.",
     "He who has a why to live can bear almost any how.",
     "To live is to suffer, to survive is to find some meaning in the suffering."
-]
+])
 
-embeddings = model.encode(docs).tolist()
-collection.add(
-    ids=["doc1", "doc2", "doc3"],
-    documents = docs,
-    embeddings=embeddings
-)
-     
-query_embedding = model.encode(["Finding purpose in suffering"]).tolist()
 results = collection.query(
-    query_embeddings=query_embedding,
+    query_texts=["Finding purpose in suffering"],
     n_results=2
 )
 print(results)
