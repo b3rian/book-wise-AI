eb39329194e5e49078df453f52bbe2f555bf29f7
b3rian <brayann.8189@gmail.com>
2025-08-11 10:20:12 +0300

    initial commits

--- vector_DB/chroma_db.py
+++ vector_DB/chroma_db.py
@@ -2,7 +2,7 @@ import os
 import chromadb
 
 # ====== CONFIG ======
-BOOKS_FOLDER = "D:\Documents\text_files" 
+BOOKS_FOLDER = r"D:\Documents\text_files" 
 DB_PATH = r"D:\Documents\chromadb"  # fixed location for persistence
 COLLECTION_NAME = "nietzsche_books"
 CHUNK_SIZE = 500  # characters per chunk
