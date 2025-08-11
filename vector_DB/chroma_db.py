4d34dfc92810c75655373957ce3271341d0df573
b3rian <brayann.8189@gmail.com>
2025-08-11 09:59:33 +0300

    initialcommits

--- vector_DB/chroma_db.py
+++ vector_DB/chroma_db.py
@@ -2,8 +2,8 @@ import os
 import chromadb
 
 # ====== CONFIG ======
-BOOKS_FOLDER = "/path/to/nietzsche_books"
-DB_PATH = "/path/to/chroma_storage/nietzsche_db"  # fixed location for persistence
+BOOKS_FOLDER = "D:\Documents\text_files" 
+DB_PATH = r"D:\Documents\chromadb"  # fixed location for persistence
 COLLECTION_NAME = "nietzsche_books"
 CHUNK_SIZE = 500  # characters per chunk
 CHUNK_OVERLAP = 50  # characters overlap between chunks
