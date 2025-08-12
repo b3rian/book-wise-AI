b3ad8bd634908b0a85f4e54f4ec39926dc773d6c
b3rian <brayann.8189@gmail.com>
2025-08-12 09:43:53 +0300

    initial commits

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Your FastAPI backend endpoint
-API_URL = "http://127.0.0.1:8000/prompt" 
+API_URL = "http://127.0.0.1:8000/prompt-stream" 
 
 st.set_page_config(page_title="Philosopher Bot", page_icon="🤖", layout="centered")
 
