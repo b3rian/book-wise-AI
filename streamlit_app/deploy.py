9b527a47ae1edd167cb099112f75b0f8aeccdf1f
b3rian <brayann.8189@gmail.com>
2025-08-13 12:16:49 +0300

    initialcommits

---
+++ streamlit_app/deploy.py
@@ -0,0 +1,27 @@
+from huggingface_hub import HfApi, upload_folder
+from pathlib import Path
+
+# Setup repo info
+username = "b3rian"
+repo_name = "streamlit-ui"
+local_dir =  Path(__file__).resolve().parent # Automatically detect current folder
+repo_type = "space"
+space_sdk = "docker" 
+
+# 1. Create the space
+api = HfApi()
+api.create_repo(
+    repo_id=f"{username}/{repo_name}",
+    repo_type=repo_type,
+    space_sdk=space_sdk,
+    exist_ok=True  # Don't fail if it already exists
+)
+
+# 2. Upload the entire folder to the space
+upload_folder(
+    repo_id=f"{username}/{repo_name}",
+    folder_path=local_dir,
+    repo_type=repo_type
+)
+
+print(f"✅ Deployed to https://huggingface.co/spaces/{username}/{repo_name}")
